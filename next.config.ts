e38be7933732f1736bd2d680e2caab63fe19d75e
jseprodi <jseprodi@gmail.com>
2025-06-18 14:46:51 -0400

    fixed imports 3

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ["assets-us-01.kc-usercontent.com"],
   },
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
