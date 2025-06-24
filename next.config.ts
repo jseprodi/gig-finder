c8614d8181f497d4cb8bd5a27faebd4d68591468
jseprodi <jseprodi@gmail.com>
2025-06-24 10:58:50 -0400

    fixed headers to allow iframe embeds

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,23 @@ const nextConfig: NextConfig = {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/custom-elements/youtube',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://app.kontent.ai",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
